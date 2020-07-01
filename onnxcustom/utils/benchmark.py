b4914450d6e4e29b2bc81b4913b34e1b87661eb0
xavier dupré <xavier.dupre@gmail.com>
2020-07-01 19:31:30 +0200

    Update benchmark.py

--- onnxcustom/utils/benchmark.py
+++ onnxcustom/utils/benchmark.py
@@ -20,7 +20,7 @@ def measure_time(stmt, context, repeat=10, number=50, div_by_number=False):
     .. runpython::
         :showcode:
 
-        from onnxcustom.tools import measure_time
+        from onnxcustom.utils import measure_time
         from math import cos
 
         res = measure_time("cos(x)", context=dict(cos=cos, x=5.))
