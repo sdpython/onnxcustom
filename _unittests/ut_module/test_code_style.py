3429c0742b8242a016f9311bb18ae1ad2b0b2459
xavier dupré <xavier.dupre@gmail.com>
2021-10-05 00:51:22 +0200

    lint

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -18,7 +18,7 @@ class TestCodeStyle(ExtTestCase):
             pylint_ignore=(
                 'C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
                 'W0201', 'W0221', 'E0632', 'R1702', 'W0212', 'W0223',
-                'W0107', "R1720", 'R1732', 'C0209'),
+                'W0107', "R1720", 'R1732', 'C0209', 'C0302'),
             skip=[])
 
     def test_style_test(self):
@@ -29,7 +29,7 @@ class TestCodeStyle(ExtTestCase):
             pylint_ignore=(
                 'C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
                 'C0111', 'W0107', 'C0111', 'R1702', 'C0415', "R1720",
-                'R1732', 'C0209'),
+                'R1732', 'C0209', 'C0302'),
             skip=[])
 
 
