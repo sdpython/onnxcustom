5759e2a3ea4e5d70c51ace07a94cf4809c3c3871
xavier dupré <xavier.dupre@gmail.com>
2021-10-04 23:02:11 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -1,62 +1,125 @@
 # -*- coding: utf-8 -*-
+import sys
 import os
-from setuptools import setup, find_packages
+import warnings
+from setuptools import setup, Extension, find_packages
+from pyquicksetup import read_version, read_readme, default_cmdclass
 
-######################
-# beginning of setup
-######################
+#########
+# settings
+#########
+
+project_var_name = "onnxcustom"
+versionPython = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
+path = "Lib/site-packages/" + project_var_name
+readme = 'README.rst'
+history = "HISTORY.rst"
+requirements = None
+
+KEYWORDS = project_var_name + ', Xavier Dupré'
+DESCRIPTION = """Extends scikit-learn with a couple of new models, transformers, metrics, plotting."""
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 5 - Production/Stable'
+]
 
 
-here = os.path.dirname(__file__)
-if here == "":
-    here = '.'
-packages = find_packages(where=here)
-package_dir = {k: os.path.join(here, k.replace(".", "/")) for k in packages}
+#######
+# data
+#######
+
+packages = find_packages()
+package_dir = {k: os.path.join('.', k.replace(".", "/")) for k in packages}
 package_data = {
-    "onnxcustom.data": ["*.csv"],
+    project_var_name + ".data": ["*.csv"],
 }
 
-try:
-    with open(os.path.join(here, "requirements.txt"), "r") as f:
-        requirements = f.read().strip(' \n\r\t').split('\n')
-except FileNotFoundError:
-    requirements = []
-if len(requirements) == 0 or requirements == ['']:
-    requirements = []
+
+def get_extensions():
+    root = os.path.abspath(os.path.dirname(__file__))
+    if sys.platform.startswith("win"):
+        extra_compile_args = None
+    else:
+        extra_compile_args = ['-std=c++11']
+
+    ext_modules = []
+
+    # mlmodel
+
+    import sklearn
+    extensions = ["direct_blas_lapack"]
+    spl = sklearn.__version__.split('.')
+    vskl = (int(spl[0]), int(spl[1]))
+    if vskl >= (0, 24):
+        extensions.append(("_piecewise_tree_regression_common",
+                           "_piecewise_tree_regression_common024"))
+    else:
+        extensions.append(("_piecewise_tree_regression_common",
+                           "_piecewise_tree_regression_common023"))
+    extensions.extend([
+        "piecewise_tree_regression_criterion",
+        "piecewise_tree_regression_criterion_linear",
+        "piecewise_tree_regression_criterion_fast",
+        "_tree_digitize",
+    ])
+
+    pattern1 = "mlinsights.%s.%s"
+    import numpy
+    for name in extensions:
+        folder = "mltree" if name == "_tree_digitize" else "mlmodel"
+        if isinstance(name, tuple):
+            m = Extension(pattern1 % (folder, name[0]),
+                          ['mlinsights/%s/%s.pyx' % (folder, name[1])],
+                          include_dirs=[numpy.get_include()],
+                          extra_compile_args=["-O3"],
+                          language='c')
+        else:
+            m = Extension(pattern1 % (folder, name),
+                          ['mlinsights/%s/%s.pyx' % (folder, name)],
+                          include_dirs=[numpy.get_include()],
+                          extra_compile_args=["-O3"],
+                          language='c')
+        ext_modules.append(m)
+
+    # cythonize
+    from Cython.Build import cythonize
+    opts = dict(boundscheck=False, cdivision=True,
+                wraparound=False, language_level=3,
+                cdivision_warnings=False, embedsignature=True,
+                initializedcheck=False)
+    ext_modules = cythonize(ext_modules, compiler_directives=opts)
+    return ext_modules
+
 
 try:
-    with open(os.path.join(here, "readme.rst"), "r", encoding='utf-8') as f:
-        long_description = "onnxcustom:" + f.read().split('onnxcustom:')[1]
-except FileNotFoundError:
-    long_description = ""
+    ext_modules = get_extensions()
+except ImportError as e:
+    warnings.warn(
+        "Unable to build C++ extension with missing dependencies %r." % e)
+    ext_modules = None
 
-version_str = '0.0.1'
-with open(os.path.join(here, 'onnxcustom/__init__.py'), "r") as f:
-    line = [_ for _ in [_.strip("\r\n ")
-                        for _ in f.readlines()]
-            if _.startswith("__version__")]
-    if len(line) > 0:
-        version_str = line[0].split('=')[1].strip('" ')
+# setup
 
-requires = ["cython", "fire", "numpy", "pandas", "scikit-learn",
-            "scipy", "sympy", 'scikit-image']
-with open(os.path.join(here, 'requirements.txt'), "r") as f:
-    requires = [_.strip() for _ in f.readlines()]
-    requires = [_ for _ in requires if _]
-
-ext_modules = []
-
-
-setup(name='onnxcustom',
-      version=version_str,
-      description="Custom ONNX operators and converters",
-      long_description=long_description,
-      author='Xavier Dupré',
-      author_email='xavier.dupre@gmail.com',
-      url='https://github.com/sdpython/onnxcustom',
-      ext_modules=ext_modules,
-      packages=packages,
-      package_dir=package_dir,
-      package_data=package_data,
-      setup_requires=requires,
-      install_requires=requires)
+setup(
+    name=project_var_name,
+    version=read_version(__file__, project_var_name),
+    author='Xavier Dupré',
+    author_email='xavier.dupre@gmail.com',
+    license="MIT",
+    url="http://www.xavierdupre.fr/app/%s/helpsphinx/index.html" % project_var_name,
+    download_url="https://github.com/sdpython/%s/" % project_var_name,
+    description=DESCRIPTION,
+    long_description=read_readme(__file__),
+    cmdclass=default_cmdclass(),
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+    setup_requires=["pyquicksetup"],
+    install_requires=["fire", "numpy", "onnx>=1.10.1", "scipy"],
+)
