20b579e3e9f79a87646acbc80645aba0148385b3
xavier dupré <xavier.dupre@gmail.com>
2022-01-01 11:50:59 +0100

    handles l1 and l2 losses for the first optimizer

--- _unittests/ut_training/test_optimizers.py
+++ _unittests/ut_training/test_optimizers.py
@@ -86,6 +86,70 @@ class TestOptimizers(ExtTestCase):
         self.assertFalse(any(map(numpy.isnan, losses)))
 
     @unittest.skipIf(TrainingSession is None, reason="not training")
+    def test_ort_gradient_optimizers_use_numpy_w_l1(self):
+        from onnxcustom.utils.onnx_orttraining import add_loss_output
+        from onnxcustom.training.optimizers import OrtGradientOptimizer
+        X, y = make_regression(  # pylint: disable=W0632
+            100, n_features=10, bias=2, random_state=0)
+        X = X.astype(numpy.float32)
+        y = y.astype(numpy.float32)
+        w = (numpy.random.rand(y.shape[0]) + 1).astype(X.dtype)
+        X_train, _, y_train, __, w_train, ___ = train_test_split(X, y, w)
+        reg = LinearRegression()
+        reg.fit(X_train, y_train, sample_weight=w_train)
+        reg.coef_ = reg.coef_.reshape((1, -1))
+        onx = to_onnx(reg, X_train, target_opset=opset,
+                      black_op={'LinearRegressor'})
+        set_model_props(onx, {'info': 'unit test'})
+        onx_loss = add_loss_output(onx, weight_name='weight', score_name='l1')
+        inits = ['intercept', 'coef']
+        train_session = OrtGradientOptimizer(
+            onx_loss, inits, learning_rate=1e-3)
+        self.assertRaise(lambda: train_session.get_state(), AttributeError)
+        train_session.fit(X_train, y_train, w_train, use_numpy=True)
+        state_tensors = train_session.get_state()
+        self.assertEqual(len(state_tensors), 2)
+        r = repr(train_session)
+        self.assertIn("OrtGradientOptimizer(model_onnx=", r)
+        self.assertIn("learning_rate='invscaling'", r)
+        losses = train_session.train_losses_
+        self.assertGreater(len(losses), 1)
+        self.assertFalse(any(map(numpy.isnan, losses)))
+
+    @unittest.skipIf(TrainingSession is None, reason="not training")
+    def test_ort_gradient_optimizers_use_numpy_w_elastic(self):
+        from onnxcustom.utils.onnx_orttraining import add_loss_output
+        from onnxcustom.training.optimizers import OrtGradientOptimizer
+        X, y = make_regression(  # pylint: disable=W0632
+            100, n_features=10, bias=2, random_state=0)
+        X = X.astype(numpy.float32)
+        y = y.astype(numpy.float32)
+        w = (numpy.random.rand(y.shape[0]) + 1).astype(X.dtype)
+        X_train, _, y_train, __, w_train, ___ = train_test_split(X, y, w)
+        reg = LinearRegression()
+        reg.fit(X_train, y_train, sample_weight=w_train)
+        reg.coef_ = reg.coef_.reshape((1, -1))
+        onx = to_onnx(reg, X_train, target_opset=opset,
+                      black_op={'LinearRegressor'})
+        set_model_props(onx, {'info': 'unit test'})
+        onx_loss = add_loss_output(
+            onx, weight_name='weight', score_name='elastic',
+            l1_weight=0.1, l2_weight=0.9)
+        inits = ['intercept', 'coef']
+        train_session = OrtGradientOptimizer(
+            onx_loss, inits, learning_rate=1e-3)
+        self.assertRaise(lambda: train_session.get_state(), AttributeError)
+        train_session.fit(X_train, y_train, w_train, use_numpy=True)
+        state_tensors = train_session.get_state()
+        self.assertEqual(len(state_tensors), 2)
+        r = repr(train_session)
+        self.assertIn("OrtGradientOptimizer(model_onnx=", r)
+        self.assertIn("learning_rate='invscaling'", r)
+        losses = train_session.train_losses_
+        self.assertGreater(len(losses), 1)
+        self.assertFalse(any(map(numpy.isnan, losses)))
+
+    @unittest.skipIf(TrainingSession is None, reason="not training")
     def test_ort_gradient_optimizers_use_numpy_saved(self):
         from onnxcustom.utils.onnx_orttraining import add_loss_output
         from onnxcustom.training.optimizers import OrtGradientOptimizer
--- onnxcustom/utils/onnx_orttraining.py
+++ onnxcustom/utils/onnx_orttraining.py
@@ -4,7 +4,9 @@
 @brief ONNX manipulations to help build ONNX gradient graphs.
 """
 from collections import OrderedDict
-from onnx.numpy_helper import to_array
+import numpy
+from onnx.mapping import TENSOR_TYPE_TO_NP_TYPE
+from onnx.numpy_helper import to_array, from_array
 from onnx.helper import (
     make_node, make_graph, make_model, make_tensor_value_info,
     set_model_props)
@@ -32,9 +34,109 @@ def _unique_name(existing_names, name, add=True):
     return name
 
 
+def _loss_l1(existing_names, elem, shape,
+             output_name, label_name,
+             weight_name, loss_name):
+    """
+    Implements loss l1.
+    """
+    diff_name = _unique_name(existing_names, "loss_diff")
+    diff2_name = _unique_name(existing_names, "loss_diff")
+    nodes = [make_node('Sub', [output_name, label_name], [diff_name]),
+             make_node('Abs', [diff_name], [diff2_name])]
+    if weight_name is not None:
+        res_name = _unique_name(existing_names, "loss_diff_weight")
+        nodes.append(
+            make_node('Mul', [diff2_name, weight_name], [res_name]))
+    else:
+        res_name = diff2_name
+    nodes.append(make_node('ReduceSum', [res_name], [loss_name]))
+
+    inputs = [make_tensor_value_info(label_name, elem, shape)]
+    if weight_name is not None:
+        inputs.append(
+            make_tensor_value_info(weight_name, elem, [shape[0]]))
+    return (
+        [], inputs, nodes,
+        [make_tensor_value_info(loss_name, elem, [1, 1])])
+
+
+def _loss_l2(existing_names, elem, shape,
+             output_name, label_name,
+             weight_name, loss_name):
+    """
+    Implements loss l2.
+    """
+    diff_name = _unique_name(existing_names, "loss_diff")
+    diff2_name = _unique_name(existing_names, "loss_diff")
+    nodes = [make_node('Sub', [output_name, label_name], [diff_name]),
+             make_node('Mul', [diff_name, diff_name], [diff2_name])]
+    if weight_name is not None:
+        res_name = _unique_name(existing_names, "loss_diff_weight")
+        nodes.append(
+            make_node('Mul', [diff2_name, weight_name], [res_name]))
+    else:
+        res_name = diff2_name
+    nodes.append(make_node('ReduceSum', [res_name], [loss_name]))
+
+    inputs = [make_tensor_value_info(label_name, elem, shape)]
+    if weight_name is not None:
+        inputs.append(
+            make_tensor_value_info(weight_name, elem, [shape[0]]))
+    return (
+        [], inputs, nodes,
+        [make_tensor_value_info(loss_name, elem, [1, 1])])
+
+
+def _loss_elastic(existing_names, elem, shape,
+                  output_name, label_name,
+                  weight_name, loss_name,
+                  l1_weight=0.5, l2_weight=0.5):
+    """
+    Implements mixture of losses l1 and l2.
+    """
+    l1_name = _unique_name(existing_names, "l1_name")
+    l2_name = _unique_name(existing_names, "l2_name")
+    dtype = TENSOR_TYPE_TO_NP_TYPE[elem]
+    onx_l1_weight = from_array(
+        numpy.array([l1_weight], dtype=dtype), name=l1_name)
+    onx_l2_weight = from_array(
+        numpy.array([l2_weight], dtype=dtype), name=l2_name)
+    inits = [onx_l1_weight, onx_l2_weight]
+
+    diff_name = _unique_name(existing_names, "loss_diff")
+    diff1_name = _unique_name(existing_names, "loss_l1")
+    diff2_name = _unique_name(existing_names, "loss_l2")
+    wl1_name = _unique_name(existing_names, "loss_l1")
+    wl2_name = _unique_name(existing_names, "loss_l2")
+    final_loss = _unique_name(existing_names, "final_loss")
+    nodes = [make_node('Sub', [output_name, label_name], [diff_name]),
+             make_node('Mul', [diff_name, diff_name], [diff2_name]),
+             make_node('Abs', [diff_name], [diff1_name]),
+             make_node('Mul', [diff1_name, l1_name], [wl1_name]),
+             make_node('Mul', [diff2_name, l2_name], [wl2_name]),
+             make_node('Add', [wl1_name, wl2_name], [final_loss]),
+             ]
+    if weight_name is not None:
+        res_name = _unique_name(existing_names, "loss_diff_weight")
+        nodes.append(
+            make_node('Mul', [final_loss, weight_name], [res_name]))
+    else:
+        res_name = final_loss
+    nodes.append(make_node('ReduceSum', [res_name], [loss_name]))
+
+    inputs = [make_tensor_value_info(label_name, elem, shape)]
+    if weight_name is not None:
+        inputs.append(
+            make_tensor_value_info(weight_name, elem, [shape[0]]))
+    return (
+        inits, inputs, nodes,
+        [make_tensor_value_info(loss_name, elem, [1, 1])])
+
+
 def add_loss_output(onx, score_name='squared_error',
                     loss_name='loss', label_name='label',
-                    weight_name=None):
+                    weight_name=None, **kwargs):
     """
     Modifies an ONNX graph to add operators to score and allow training.
 
@@ -48,9 +150,14 @@ def add_loss_output(onx, score_name='squared_error',
 
     Possible values for *score_name*:
 
-    * `'squared_error'`: :math:`\\sum_i{(f(x_i)-y_i)^2}` or
-        :math:`\\sum_i{w_i (f(x_i)-y_i)^2}` if *weight_name*
-        is not None
+    * `'squared_error'` or `'l2`': :math:`\\sum_i{(f(x_i)-y_i)^2}` or
+      :math:`\\sum_i{w_i (f(x_i)-y_i)^2}` if *weight_name*
+      is not None
+    * `'absolute_error'` or `'l1`': :math:`\\sum_i{|f(x_i)-y_i|}` or
+      :math:`\\sum_i{w_i |f(x_i)-y_i|}` if *weight_name*
+      is not None
+    * `'elastic'`: mixture of losses, kwargs must define
+      *l1_weight* and *l2_weight*, undefined, default value are 0.5
 
     See example :ref:`l-orttraining-nn-gpu`.
     """
@@ -72,34 +179,28 @@ def add_loss_output(onx, score_name='squared_error',
     for d in onx.graph.output[0].type.tensor_type.shape.dim:
         shape.append(d.dim_value if d.dim_value > 0 else None)
 
-    if score_name == 'squared_error':
-        diff_name = _unique_name(existing_names, "loss_diff")
-        diff2_name = _unique_name(existing_names, "loss_diff")
-        nodes = [make_node('Sub', [output_name, label_name], [diff_name]),
-                 make_node('Mul', [diff_name, diff_name], [diff2_name])]
-        if weight_name is not None:
-            res_name = _unique_name(existing_names, "loss_diff_weight")
-            nodes.append(
-                make_node('Mul', [diff2_name, weight_name], [res_name]))
-        else:
-            res_name = diff2_name
-        nodes.append(make_node('ReduceSum', [res_name], [loss_name]))
-
-        inputs = [make_tensor_value_info(label_name, elem, shape)]
-        if weight_name is not None:
-            inputs.append(
-                make_tensor_value_info(weight_name, elem, [shape[0]]))
-        outputs = [make_tensor_value_info('loss', elem, [1, 1])]
+    if score_name in ('squared_error', 'l2'):
+        inits, inputs, nodes, outputs = _loss_l2(
+            existing_names, elem, shape, output_name, label_name,
+            weight_name, loss_name)
+    elif score_name in ('absolute_error', 'l1'):
+        inits, inputs, nodes, outputs = _loss_l1(
+            existing_names, elem, shape, output_name, label_name,
+            weight_name, loss_name)
+    elif score_name == 'elastic':
+        inits, inputs, nodes, outputs = _loss_elastic(
+            existing_names, elem, shape, output_name, label_name,
+            weight_name, loss_name, **kwargs)
     else:
         raise NotImplementedError(  # pragma: no cover
             "Unexpected %r value for score_name." % score_name)
-
+    inits = list(onx.graph.initializer) + inits
     graph = make_graph(
         list(onx.graph.node) + nodes,
         onx.graph.name,
         list(onx.graph.input) + inputs,
         outputs + list(onx.graph.output),
-        onx.graph.initializer)
+        inits)
     onnx_model = make_model(graph)
     onnx_model.ir_version = onx.ir_version
     onnx_model.producer_name = onx.producer_name
