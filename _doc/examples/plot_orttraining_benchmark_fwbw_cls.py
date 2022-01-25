0c10eeec1e51e90bd9f7a43c19879957cfc97445
xavier dupré <xavier.dupre@gmail.com>
2022-01-25 10:16:12 +0100

    Update plot_orttraining_benchmark_fwbw_cls.py

--- _doc/examples/plot_orttraining_benchmark_fwbw_cls.py
+++ _doc/examples/plot_orttraining_benchmark_fwbw_cls.py
@@ -205,14 +205,15 @@ with warnings.catch_warnings():
 
 onx = to_onnx(nn, X_train[:1].astype(numpy.float32), target_opset=15,
               options={'zipmap': False, 'nocl': True})
-print(onnx_simple_text_plot(onx))
-
 onx = select_model_inputs_outputs(
     onx, outputs=["add_result"], infer_shapes=True)
-print(onnx_simple_text_plot(onx))
-
 onx = onnx_rename_weights(onx)
-print(onnx_simple_text_plot(onx))
+
+try:
+    print(onnx_simple_text_plot(onx))
+except RuntimeError as e:
+    print("You should upgrade mlprodict.")
+    print(e)
 
 train_session = OrtGradientForwardBackwardOptimizer(
     onx, device='cpu', warm_start=False,
