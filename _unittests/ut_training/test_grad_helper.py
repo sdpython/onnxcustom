326fa47bb0117a4b7676849b3e3418772e748eb5
xavier dupré <xavier.dupre@gmail.com>
2022-02-12 12:49:53 +0100

    update unit test and documentation

--- _unittests/ut_training/test_grad_helper.py
+++ _unittests/ut_training/test_grad_helper.py
@@ -11,13 +11,42 @@ try:
 except ImportError:
     # onnxruntime not training
     TrainingSession = None
-from skl2onnx.algebra.onnx_ops import OnnxAdd  # pylint: disable=E0611
+from onnxruntime.capi.onnxruntime_pybind11_state import (  # pylint: disable=E0611
+    Fail as OrtFail)
+from skl2onnx.algebra.onnx_ops import (  # pylint: disable=E0611
+    OnnxAdd, OnnxMul, OnnxIdentity)
 from skl2onnx.common.data_types import FloatTensorType
+from mlprodict.onnxrt.validate.validate_latency import random_feed
+from mlprodict.onnxrt import OnnxInference
 from onnxcustom import __max_supported_opset__ as opset
 from onnxcustom.training.grad_helper import onnx_derivative
 
 
 class TestGradHelper(ExtTestCase):
+
+    def check_runtime(self, onx, name):
+        feeds = random_feed(onx.graph.input)
+        n = 0
+        for _, v in feeds.items():
+            if v.shape[0] > 5:
+                n += 1
+        if n == 0:
+            raise AssertionError(
+                "No input with more than 5 rows: %r." % feeds)
+        sess = InferenceSession(onx.SerializeToString())
+        try:
+            got = sess.run(None, feeds)
+        except OrtFail as e:
+            with open("fail_%s.onnx" % name, "wb") as f:
+                f.write(onx.SerializeToString())
+            raise AssertionError(
+                "Unable to run onnx graph %r." % ("fail_%s.onnx" % name)) from e
+        oinf = OnnxInference(onx)
+        pygot = oinf.run(feeds)
+        output_names = [o.name for o in onx.graph.output]
+        self.assertGreater(len(output_names), 1)
+        for i, o in enumerate(output_names):
+            self.assertEqualArray(got[i], pygot[o])
 
     @classmethod
     def setUpClass(cls):
@@ -26,17 +55,49 @@ class TestGradHelper(ExtTestCase):
         logging.basicConfig(level=logging.WARNING)
 
     @unittest.skipIf(TrainingSession is None, reason="not training")
+    def test_grad_helper_keep_yield(self):
+        opv = opset
+        node = OnnxAdd('X', numpy.array([1], dtype=numpy.float32),
+                       op_version=opv, output_names=['Y'])
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
+                           target_opset=opv)
+        new_onx = onnx_derivative(onx, out_yield_op=False)
+        types = set(n.op_type for n in new_onx.graph.node)  # pylint: disable=E1101
+        self.assertIn('YieldOp', types)
+
+    @unittest.skipIf(TrainingSession is None, reason="not training")
     def test_grad_helper(self):
         opv = opset
         node = OnnxAdd('X', numpy.array([1], dtype=numpy.float32),
                        op_version=opv, output_names=['Y'])
-        onx = node.to_onnx({'X': FloatTensorType([None, None])},
-                           {'Y': FloatTensorType([None, None])},
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
                            target_opset=opv)
-
         new_onx = onnx_derivative(onx)
-        sess = InferenceSession(new_onx.SerializeToString())
-        self.assertNotEmpty(sess)
+        self.check_runtime(new_onx, 'test_grad_helper')
+
+    @unittest.skipIf(TrainingSession is None, reason="not training")
+    def test_grad_helper_mul(self):
+        opv = opset
+        xi = OnnxIdentity('X', op_version=opv)
+        node = OnnxMul(xi, xi, op_version=opv, output_names=['Y'])
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
+                           target_opset=opv)
+        new_onx = onnx_derivative(onx)
+        self.check_runtime(new_onx, 'test_grad_helper_mul')
+
+    @unittest.skipIf(TrainingSession is None, reason="not training")
+    def test_grad_helper_noweight(self):
+        opv = opset
+        node = OnnxAdd('X', numpy.array([1], dtype=numpy.float32),
+                       op_version=opv, output_names=['Y'])
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
+                           target_opset=opv)
+        new_onx = onnx_derivative(onx, weights=[])
+        self.check_runtime(new_onx, 'test_grad_helper_noweight')
 
 
 if __name__ == "__main__":
--- onnxcustom/training/grad_helper.py
+++ onnxcustom/training/grad_helper.py
@@ -11,14 +11,93 @@ from onnxruntime.capi._pybind_state import (  # pylint: disable=E0611
 from ..utils.orttraining_helper import get_train_initializer
 
 
-def onnx_derivative(onx, weights=None, include_inputs=True):
+def onnx_derivative(onx, weights=None, inputs=None,
+                    out_yield_op=True, keep_output=True):
     """
     Builds the gradient for an onnx graph.
 
     :param onx: onnx graph
-    :param weights: gradient against those weights
-    :param include_inputs: include inputs
+    :param weights: gradient against those weights, None for all real weights
+    :param inputs: gradient against inputs, None for all real inputs
+    :param out_yield_op: promotes yield operator as output
+    :param keep_output: keep the function output or only returns the
+        gradient, this parameter is unused if *out_yield_op* is False
     :return: onnx graph
+
+    The function calls :epkg:`OrtModuleGraphBuilderConfiguration`
+    from :epkg:`onnxruntime-training`. This graph is meant to be used
+    with :epkg:`OrtGradientForwardBackward` and includes
+    operator `YieldOp`. That's the graph looks this way:
+
+    .. gdot::
+        :script: DOT-SECTION
+
+        import numpy
+        from skl2onnx.algebra.onnx_ops import (  # pylint: disable=E0611
+            OnnxAdd, OnnxMul, OnnxIdentity)
+        from skl2onnx.common.data_types import FloatTensorType
+        from mlprodict.onnxrt import OnnxInference
+        from onnxcustom.training.grad_helper import onnx_derivative
+        from onnxcustom import __max_supported_opset__ as opv
+
+        node = OnnxAdd('X', numpy.array([1], dtype=numpy.float32),
+                       op_version=opv, output_names=['Y'])
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
+                           target_opset=opv)
+        new_onx = onnx_derivative(onx, out_yield_op=False)
+
+        oinf = OnnxInference(new_onx)
+        print("DOT-SECTION", oinf.to_dot())
+
+    These operators are the outputs of the
+    initial graph and must be replaced by the gradient of these
+    outputs to compute the gradient of the weights and the inputs.
+    After they are replaced, it looks this way:
+
+    .. gdot::
+        :script: DOT-SECTION
+
+        import numpy
+        from skl2onnx.algebra.onnx_ops import (  # pylint: disable=E0611
+            OnnxAdd, OnnxMul, OnnxIdentity)
+        from skl2onnx.common.data_types import FloatTensorType
+        from mlprodict.onnxrt import OnnxInference
+        from onnxcustom.training.grad_helper import onnx_derivative
+        from onnxcustom import __max_supported_opset__ as opv
+
+        node = OnnxAdd('X', numpy.array([1], dtype=numpy.float32),
+                       op_version=opv, output_names=['Y'])
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
+                           target_opset=opv)
+        new_onx = onnx_derivative(onx, out_yield_op=True, keep_output=False)
+
+        oinf = OnnxInference(new_onx)
+        print("DOT-SECTION", oinf.to_dot())
+
+    The user can still compute the outputs.
+
+    .. gdot::
+        :script: DOT-SECTION
+
+        import numpy
+        from skl2onnx.algebra.onnx_ops import (  # pylint: disable=E0611
+            OnnxAdd, OnnxMul, OnnxIdentity)
+        from skl2onnx.common.data_types import FloatTensorType
+        from mlprodict.onnxrt import OnnxInference
+        from onnxcustom.training.grad_helper import onnx_derivative
+        from onnxcustom import __max_supported_opset__ as opv
+
+        node = OnnxAdd('X', numpy.array([1], dtype=numpy.float32),
+                       op_version=opv, output_names=['Y'])
+        onx = node.to_onnx({'X': FloatTensorType([None, 10])},
+                           {'Y': FloatTensorType([None, 10])},
+                           target_opset=opv)
+        new_onx = onnx_derivative(onx, out_yield_op=True, keep_output=True)
+
+        oinf = OnnxInference(new_onx)
+        print("DOT-SECTION", oinf.to_dot())
     """
     if weights is None:
         inits = get_train_initializer(onx)
@@ -28,9 +107,21 @@ def onnx_derivative(onx, weights=None, include_inputs=True):
     config = OrtModuleGraphBuilderConfiguration()
     config.initializer_names = weights
     config.initializer_names_to_train = weights
-    if include_inputs:
-        input_names = [n.name for n in onx.graph.input]
-        config.input_names_require_grad = input_names
+    if inputs is None:
+        inputs_name = []
+        for i in onx.graph.input:
+            try:
+                elem_type = i.type.tensor_type.elem_type
+            except AttributeError:
+                # not a vector
+                continue
+            if elem_type in (
+                    onnx.TensorProto.FLOAT16,  # pylint: disable=E1101
+                    onnx.TensorProto.FLOAT,  # pylint: disable=E1101
+                    onnx.TensorProto.DOUBLE):  # pylint: disable=E1101
+                inputs_name.append(i.name)
+        if len(inputs_name) > 0:
+            config.input_names_require_grad = inputs_name
     config.build_gradient_graph = True
 
     p = TrainingGraphTransformerConfiguration()
@@ -40,4 +131,12 @@ def onnx_derivative(onx, weights=None, include_inputs=True):
     builder.build()
     train_onnx_model_serialized = builder.get_model()
     # optimized_pre_grad_model = builder.get_inference_optimized_model()
-    return onnx.load(BytesIO(train_onnx_model_serialized))
+    grad_yield = onnx.load(BytesIO(train_onnx_model_serialized))
+    if not out_yield_op:
+        return grad_yield
+
+    yields_op = [node for node in grad_yield.graph.node  # pylint: disable=E1101
+                 if node.op_type == 'YieldOp']
+    if len(yields_op) == 0:
+        raise RuntimeError(  # pragma: no cover
+            "No YieldOp was found. The input graph must be wrong.")
