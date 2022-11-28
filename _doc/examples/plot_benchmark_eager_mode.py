693c598489969608a053b8a0d4b860f5dd3e05cd
Xavier Dupre <xadupre@microsoft.com>
2022-11-28 13:25:32 +0000

    add gpu

--- _doc/examples/plot_benchmark_eager_mode.py
+++ _doc/examples/plot_benchmark_eager_mode.py
@@ -45,7 +45,8 @@ from onnx.numpy_helper import from_array
 from onnx.helper import (
     make_model, make_node,
     make_graph, make_tensor_value_info)
-from onnxruntime import InferenceSession, __version__ as ort_version
+from onnxruntime import (
+    get_all_providers, InferenceSession, __version__ as ort_version)
 from onnxruntime.capi._pybind_state import (  # pylint: disable=E0611
     OrtDevice as C_OrtDevice,
     OrtMemType, OrtValue as C_OrtValue)
@@ -86,6 +87,20 @@ graph = make_graph([
 onnx_add2 = make_model(graph)
 sess_add2 = InferenceSession(onnx_add2.SerializeToString(),
                              providers=["CPUExecutionProvider"])
+
+############################################
+# Let's consider GPU as well.
+
+has_cuda = "CUDAExecutionProvider" in get_all_providers()
+if has_cuda:
+    sess_add_gpu = InferenceSession(onnx_add.SerializeToString(),
+                                    providers=["CUDAExecutionProvider"])
+    sess_add2_gpu = InferenceSession(onnx_add2.SerializeToString(),
+                                     providers=["CUDAExecutionProvider"])
+else:
+    print("No GPU or one GPU was detected.")
+    sess_add_gpu = None
+    sess_add2_gpu = None
 
 ############################################
 # The functions to test
@@ -129,6 +144,24 @@ def f_ort_ov(X):
     return Z
 
 
+if sess_add_gpu is not None:
+
+    def f_ort_ov_eager_gpu(X):
+        "ort-ov-eager-gpu"
+        T = sess_add_gpu._sess.run_with_ort_values({'X': X}, ['Z'], None)[0]
+        Z = sess_add_gpu._sess.run_with_ort_values({'X': T}, ['Z'], None)[0]
+        return Z
+
+
+    def f_ort_ov_gpu(X):
+        "ort-ov-gpu"
+        Z = sess_add2_gpu._sess.run_with_ort_values({'X': X}, ['Z'], None)[0]
+        return Z
+
+else:
+    f_ort_ov_eager_gpu = None
+    f_ort_ov_gpu = None
+
 X = numpy.random.rand(10, CST.shape[1]).astype(CST.dtype)
 
 device = C_OrtDevice(C_OrtDevice.cpu(), OrtMemType.DEFAULT, 0)
@@ -141,6 +174,13 @@ Ys = [
     f_ort_ov_eager(Xov),
     f_ort_ov(Xov),
 ]
+if sess_add_gpu is not None:
+    device_gpu = C_OrtDevice(C_OrtDevice.cuda(), OrtMemType.DEFAULT, 0)
+    Xov_gpu = C_OrtValue.ortvalue_from_numpy(X, device_gpu)
+    Ys.extend([
+        f_ort_ov_eager_gpu(Xov_gpu),
+        f_ort_ov_gpu(Xov_gpu),
+    ])
 
 for i in range(1, len(Ys)):
     try:
@@ -158,17 +198,28 @@ for i in range(1, len(Ys)):
 
 
 def benchmark(repeat=100):
-    fcts = [f_numpy, f_ort_eager, f_ort, f_ort_ov_eager, f_ort_ov]
+    fcts = [f_numpy, f_ort_eager, f_ort, f_ort_ov_eager, f_ort_ov,
+            f_ort_ov_eager_gpu, f_ort_ov_gpu]
     data = []
     for N in tqdm([1, 2, 5, 10, 20, 50, 100, 200, 500,
                    1000, 2000, 5000, 10000, 20000]):
         X = numpy.random.rand(N, CST.shape[1]).astype(CST.dtype)
         device = C_OrtDevice(C_OrtDevice.cpu(), OrtMemType.DEFAULT, 0)
         Xov = C_OrtValue.ortvalue_from_numpy(X, device)
+        if f_ort_ov_gpu is not None:
+            device_gpu = C_OrtDevice(C_OrtDevice.cuda(), OrtMemType.DEFAULT, 0)
+            Xov_gpu = C_OrtValue.ortvalue_from_numpy(X, device_gpu)
 
         for f in fcts:
+            if f is None:
+                continue
             obs = {'name': f.__doc__, "N": N}
-            if "-ov" in f.__doc__:
+            if "-gpu" in f.__doc__:
+                begin = time.perf_counter()
+                for r in range(repeat):
+                    _ = f(Xov_gpu)
+                end = time.perf_counter() - begin
+            elif "-ov" in f.__doc__:
                 begin = time.perf_counter()
                 for r in range(repeat):
                     _ = f(Xov)
@@ -185,6 +236,7 @@ def benchmark(repeat=100):
 
 
 df = benchmark()
+df.to_csv("plot_benchmark_eager_mode.csv", index=False)
 df
 
 
@@ -192,28 +244,53 @@ df
 # Graphs
 # ++++++
 
-fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+def make_graph(df):
+    fig, ax = plt.subplots(2, 3, figsize=(12, 8))
 
-piv = df.pivot(index="N", columns="name", values="time")
-piv.plot(ax=ax[0], title="Time(s) per execution", logy=True, logx=True)
-piv2 = piv / piv.index.values.reshape((-1, 1))
-piv2.plot(ax=ax[1], title="Time(s) per execution / N", logx=True)
-piv3 = piv / piv["numpy"].values.reshape((-1, 1))
-piv3.plot(ax=ax[2], title="Ratio against numpy (lower is better)",
-          logy=True, logx=True)
+    piv_all = df.pivot(index="N", columns="name", values="time")
 
+    # no gpu
+    piv = piv_all[[c for c in piv_all.columns if "gpu" not in c]].copy()
+    piv.plot(ax=ax[0, 0], title="Time(s) per execution", logy=True, logx=True)
+    piv2 = piv / piv.index.values.reshape((-1, 1))
+    piv2.plot(ax=ax[0, 1], title="Time(s) per execution / N", logx=True)
+    piv3 = piv / piv["numpy"].values.reshape((-1, 1))
+    piv3.plot(ax=ax[0, 2], title="Ratio against numpy (lower is better)",
+            logy=True, logx=True)
+
+    # ort value
+    piv = piv_all[[c for c in piv_all.columns if "ov" in c or "numpy" in c]].copy()
+    piv.plot(ax=ax[1, 0], title="Time(s) per execution", logy=True, logx=True)
+    piv2 = piv / piv.index.values.reshape((-1, 1))
+    piv2.plot(ax=ax[1, 1], title="Time(s) per execution / N", logx=True)
+    piv3 = piv / piv["numpy"].values.reshape((-1, 1))
+    piv3.plot(ax=ax[1, 2], title="Ratio against numpy (lower is better)",
+            logy=True, logx=True)
+    return fig, ax
+
+
+fig, ax = make_graph(df)
 
 ###################################
 # Conclusion
 # ++++++++++
 #
-# The eager mode is slower than numpy for small arrays than is faster.
+# The eager mode is slower than numpy for small arrays then is faster.
 # This is probably due to :epkg:`pybind11` binding when numpy
 # is using the direct python API. This could be improved by using :epkg:`cython`.
 # Eager mode must use :epkg:`OrtValue`. It is faster and it reduces the differences
-# between using two additions in a single graph or two graphs of a single addition.
+# between using two additions in a single graph or two graphs of a single addition
+# on CPU. On GPU, it is still faster but eager mode is significantly slower.
+
+if not has_cuda:
+    print("With GPU")
+    df = pandas.read_csv("data/eager_mode.csv")
+    _, ax = make_graph(df)
+else:
+    ax = None
+ax
 
 print(f"onnxruntime.__version__ = {ort_version!r}")
 
-
-plt.show()
+fig.savefig("eager.png")
+# plt.show()
